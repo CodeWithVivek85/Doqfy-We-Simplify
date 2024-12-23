b74a1ab919ff42360848bf5fb32c3cd25454a360
CodeWithVivek85 <vivek.kumar4143@gmail.com>
2024-12-23 13:50:25 +0530

    cat upload

--- src/main/java/objectRepositary/CatUploadPage.java
+++ src/main/java/objectRepositary/CatUploadPage.java
@@ -1,5 +1,34 @@
 package objectRepositary;
+/**
+ * @author Vivek
+ */
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class CatUploadPage {
+	
+	@FindBy(xpath = "//div[@class=\"row col-lg-4 col-sm-4 col-md-4 upload-component\"]/descendant::span[.='Upload']")
+	private WebElement firstUploadButton;
+	@FindBy(xpath = "//button[.='Choose Files ']")
+	private WebElement chooseFileToUpload;
+	@FindBy(xpath = "//div[@class=\"row mt-4\"]/descendant::span[.='Upload']")
+	private WebElement finalUploadButtom;
+	@FindBy(xpath = "//span[.='Next']")
+	private WebElement nextButton;
+	
+	public WebElement getFirstUploadButton() {
+		return firstUploadButton;
+	}
+	public WebElement getChooseFileToUpload() {
+		return chooseFileToUpload;
+	}
+	public WebElement getFinalUploadButtom() {
+		return finalUploadButtom;
+	}
+	public WebElement getNextButton() {
+		return nextButton;
+	}
+	
+	
 
 }
